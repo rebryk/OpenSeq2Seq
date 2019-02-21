c1ed7b06d91648b2387d7e8a0b1184bb616db8bc
Yurii Rebryk <y.a.rebryk@gmail.com>
2019-02-21 11:49:24 -0800

    Fix import error
    Signed-off-by: Yurii Rebryk <y.a.rebryk@gmail.com>

--- open_seq2seq/losses/__init__.py
+++ open_seq2seq/losses/__init__.py
@@ -2,10 +2,11 @@
 """
 Losses to be used in seq2seq models
 """
+from .cross_entropy_loss import CrossEntropyLoss
+from .ctc_loss import CTCLoss
+from .jca_loss import MultiTaskCTCEntropyLoss
 from .sequence_loss import BasicSequenceLoss, CrossEntropyWithSmoothing, \
     PaddedCrossEntropyLossWithSmoothing, BasicSampledSequenceLoss
-from .ctc_loss import CTCLoss
-from .cross_entropy_loss import CrossEntropyLoss
 from .tacotron_loss import TacotronLoss
+from .transformer_tts_loss import TransformerTTSLoss
 from .wavenet_loss import WavenetLoss
-from .jca_loss import MultiTaskCTCEntropyLoss
