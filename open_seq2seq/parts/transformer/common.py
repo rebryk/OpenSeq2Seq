06da10be9007a81b472328c79cce0408f3fe42d3
Yurii Rebryk <y.a.rebryk@gmail.com>
2019-04-11 10:04:22 -0700

    Revert "Set gamma to zero"
    This reverts commit f07b60154595a7e4250d2020979345d5fe33c5ea.

--- open_seq2seq/parts/transformer/common.py
+++ open_seq2seq/parts/transformer/common.py
@@ -49,7 +49,7 @@ class LayerNormalization(tf.layers.Layer):
 
   def build(self, _):
     self.scale = tf.get_variable("layer_norm_scale", [self.hidden_size],
-                                 initializer= tf.keras.initializers.Zeros(),
+                                 initializer= tf.keras.initializers.Ones(),
                                  dtype=tf.float32)
     self.bias = tf.get_variable("layer_norm_bias", [self.hidden_size],
                                  initializer=tf.keras.initializers.Zeros(),
